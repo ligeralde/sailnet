Minor bug fix and sparsenet scaling
-model.py: fixes scaling factor for rfw
-sparsenet: adds parameter descriptions and adds sigma parameter for scaling activities. renames learning rate to eta to match original paper.

sailnet/model.py
@@ -221,7 +221,7 @@ class SAILnet(dictlearner.DictLearner):
                 mask = self.W[np.triu_indices(1024,k=1)] > 0
                 W = self.W[np.triu_indices(1024,k=1)][mask]
                 rfoverlaps = self.Q.dot(self.Q.T)[np.triu_indices(1024,k=1)][mask]
-                if t % self.rfw_store_factor == 0:
+                if t % self.store_every*self.rfw_store_factor == 0:
                     self.Whistory.append(W)
                     self.rfoverlaphistory.append(rfoverlaps)
                     if len(W) == 0: